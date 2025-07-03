73066a70f629ae97091fb5e37de646785bd1094e
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-07-03 22:43:57 +0500

    feat: edit docker compose for redis
    Signed-off-by: idkwhttsay <danil.tatarinov.00@gmail.com>

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -n "$SSL_CERT_PATH" ] && [ -n "$SSL_KEY_PATH" ]; then
-  exec uvicorn src.main:app --host 0.0.0.0 --port 8443 --ssl-keyfile "$SSL_KEY_PATH" --ssl-certfile "$SSL_CERT_PATH"
-else
-  exec uvicorn src.main:app --host 0.0.0.0 --port 8000
-fi
